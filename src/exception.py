4ca6585ab8380068fa823bb64eb628eba479728d
Mit Patel <patel.m9521@gmail.com>
2025-06-22 13:11:52 +0100

    updated exception and logging

--- src/exception.py
+++ src/exception.py
@@ -15,9 +15,16 @@ class CustomError(Exception):
     def __init__(self, error_message: str, error_detail: sys) -> None:
         super().__init__(error_message)
         self.error_message = error_message_detail(
-            error_message,
-            error_detail=error_detail,
+            error_message, error_detail=error_detail,
         )
 
     def __str__(self) -> str:
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomError(e, sys)
