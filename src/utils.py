c888fdbfb5694045fb10a4cbaae91c5c6f9b1e89
Mit Patel <patel.m9521@gmail.com>
2025-06-22 19:50:14 +0100

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomError
 
@@ -20,13 +21,18 @@ def save_object(file_path: str, obj: object) -> None:
         raise CustomError(e, sys)
 
 
-def evaluate_models(X_train: object, y_train: object, X_test: object, y_test: object, models: dict) -> dict:
+def evaluate_models(X_train: object, y_train: object, X_test: object, y_test: object, models: dict, param: dict) -> dict:
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
